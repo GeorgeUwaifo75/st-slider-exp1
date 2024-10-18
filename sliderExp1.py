ec7a6b4c9f14b34938477531862e713604322766
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-10-18 23:09:15 +0100

    Update sliderExp1.py
    I just added the import streamlit as st.  It wasn't there.

--- sliderExp1.py
+++ sliderExp1.py
@@ -1,13 +1,12 @@
 import pandas as pd
 import numpy as np
+import streamlit as st
 
 # Create a sample dataframe
 df = pd.DataFrame(np.random.randn(10, 2), columns=['A', 'B'])
 
 # Add a slider
-#slider_val = st.slider('Select a range', 0, 10)
-
-slider_val = st.slider('Select a range')
+slider_val = st.slider('Select a range', 0, 10)
 
 # Filter the dataframe
 filtered_df = df[df['A'] > slider_val]
