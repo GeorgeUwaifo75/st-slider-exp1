94dd3824fd205ddc5c96217b4e92da32fed21698
GeorgeUwaifo75 <george.uwaifo@gmail.com>
2024-10-18 23:05:57 +0100

    Update sliderExp1.py
    This is my latest change to my code.  I modified slider

--- sliderExp1.py
+++ sliderExp1.py
@@ -5,7 +5,9 @@ import numpy as np
 df = pd.DataFrame(np.random.randn(10, 2), columns=['A', 'B'])
 
 # Add a slider
-slider_val = st.slider('Select a range', 0, 10)
+#slider_val = st.slider('Select a range', 0, 10)
+
+slider_val = st.slider('Select a range')
 
 # Filter the dataframe
 filtered_df = df[df['A'] > slider_val]
